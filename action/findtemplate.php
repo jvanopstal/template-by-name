db87549fa9e667b128818100083fb29c7c2fd3b6
jvanopstal <jvanopstal@mznl.be>
2012-06-04 12:59:41 +0200

    Change the asterix to a tilde to make this plugin usable on windows systems. Removed a bug when using te specific filename.

--- action/findtemplate.php
+++ action/findtemplate.php
@@ -36,44 +36,43 @@ class action_plugin_templatebyname_findtemplate extends DokuWiki_Action_Plugin {
 				$blnFirst = true;
 				$blnFirstDir = true;
 				while (strLen($path) >= $len){
-					if($blnFirst == true && @file_exists($path.'/_'.noNS($event->data['id']))){
-						$event->data['tplfile'] = $path.'/_'.noNS($event->data['id']);
-						break;
-					}
-					elseif(@file_exists($path.'/__'.noNS($event->data['id']))){
-						$event->data['tplfile'] = $path.'/__'.noNS($event->data['id']);
-						break;
-					}
-					elseif($blnFirst == true && @file_exists($path.'/_template.txt')){
-						$event->data['tplfile'] = $path.'/_template.txt';
-						break;
-					}
-					elseif($blnFirst == false && $blnFirstDir == true && @file_exists($path.'/*_'.$dir.'.txt') && noNS($event->data['id']) == 'start'){
-						$event->data['tplfile'] = $path.'/*_'.$dir.'.txt';
-						break;
-					}
-					elseif($blnFirst == false && $blnFirstDir == true && @file_exists($path.'/*'.$dir.'.txt')){
-						$event->data['tplfile'] = $path.'/*'.$dir.'.txt';
-						break;
-					}
-					elseif($blnFirst == false && @file_exists($path.'/**_'.$dir.'.txt') && noNS($event->data['id']) == 'start'){
-						$event->data['tplfile'] = $path.'/**_'.$dir.'.txt';
-						break;
-					}
-					elseif($blnFirst == false && @file_exists($path.'/**'.$dir.'.txt')){
-						$event->data['tplfile'] = $path.'/**'.$dir.'.txt';
-						break;
-					}
-					elseif(@file_exists($path.'/__template.txt')){
-						$event->data['tplfile'] = $path.'/__template.txt';
-						break;
-					}
-						
-					$path = substr($path, 0, strrpos($path, '/'));
-					if($blnFirst == false){
-						$blnFirstDir = false;
-					}
-					$blnFirst = false;
+									if($blnFirst == true && @file_exists($path.'/_'.noNS($event->data['id']).'.txt')){
+										$event->data['tplfile'] = $path.'/_'.noNS($event->data['id'].'.txt');
+										break;
+									}
+									elseif(@file_exists($path.'/__'.noNS($event->data['id']).'.txt')){
+										$event->data['tplfile'] = $path.'/__'.noNS($event->data['id'].'.txt');
+										break;
+									}
+									elseif($blnFirst == true && @file_exists($path.'/_template.txt')){
+										$event->data['tplfile'] = $path.'/_template.txt';
+										break;
+									}
+									elseif($blnFirst == false && $blnFirstDir == true && @file_exists($path.'/~_'.$dir.'.txt') && noNS($event->data['id']) == 'start'){
+										$event->data['tplfile'] = $path.'/~_'.$dir.'.txt';
+										break;
+									}
+									elseif($blnFirst == false && $blnFirstDir == true && @file_exists($path.'/~'.$dir.'.txt')){
+										$event->data['tplfile'] = $path.'/~'.$dir.'.txt';
+										break;
+									}
+									elseif($blnFirst == false && @file_exists($path.'/~~_'.$dir.'.txt') && noNS($event->data['id']) == 'start'){
+										$event->data['tplfile'] = $path.'/~~_'.$dir.'.txt';
+										break;
+									}
+									elseif($blnFirst == false && @file_exists($path.'/~~'.$dir.'.txt')){
+										$event->data['tplfile'] = $path.'/~~'.$dir.'.txt';
+										break;
+									}
+									elseif(@file_exists($path.'/__template.txt')){
+										$event->data['tplfile'] = $path.'/__template.txt';
+										break;
+									}
+									$path = substr($path, 0, strrpos($path, '/'));
+									if($blnFirst == false){
+										$blnFirstDir = false;
+									}
+									$blnFirst = false;
 				}
 			}
 			$event->data['tpl'] = io_readFile($event->data['tplfile']);
